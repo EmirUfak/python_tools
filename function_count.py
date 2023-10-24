085002de8e3bef81e37bad62fe2d67c257d5038e
Emir Ufak <emir.ufak@gmail.com>
2023-10-24 16:28:21 +0300

    Create function_count.py

---
+++ function_count.py
@@ -0,0 +1,24 @@
+a = "aldfjasdlfasdf@adlsfkşadsf"
+b = "ads.kfjşaldksfasf"
+c = "fskkjflşsdflkdsfjkdsfjlkdsf"
+
+x = a.count("@")
+y = b.count("@")
+z = c.count("@")
+
+if not x == 1:
+    print("yok")
+else:
+    print("var")
+
+if not y == 1:
+    print("yok")
+else:
+    print("var")
+
+
+if not z == 1:
+    print("yok")
+else:
+    print("var")
+
